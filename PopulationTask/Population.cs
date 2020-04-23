4bef565c30282b696fa71ed44bada9db690dc134
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2020-04-23 13:47:29 +0300

    add-task-and-tests-projects

---
+++ PopulationTask/Population.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PopulationTask
+{
+    public static class Population
+    {
+        public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
+        {
+            throw new NotImplementedException("You need to implement this function.");
+        }
+    }
+}
