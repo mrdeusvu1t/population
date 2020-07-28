cc3dc061eda70eaaa0b99339aab1cf41d3712243
Anzhelika Kravchuk <anzhelika.kravchuk@gmail.com>
2020-07-28 18:30:03 +0000

    Replace Population.cs

--- PopulationTask/Population.cs
+++ PopulationTask/Population.cs
@@ -4,6 +4,24 @@ namespace PopulationTask
 {
     public static class Population
     {
+        /// <summary>
+        /// Calculate the count of years which the town need to see its population greater or equal to currentPopulation inhabitants.
+        /// </summary>
+        /// <param name="initialPopulation">The population at the beginning of a year.</param>
+        /// <param name="percent">The percentage of growth per year.</param>
+        /// <param name="visitors">The visitors (new inhabitants per year) who come to live in the town.</param>
+        /// <param name="currentPopulation">The population at present.</param>
+        /// <returns>The count of years which the town need to see its population greater or equal to currentPopulation inhabitants.</returns>
+        /// <exception cref="ArgumentException">
+        /// Throw if initial population is less or equals zero
+        /// - or -
+        /// the count of visitors cannot be less zero
+        /// - or -
+        /// the current population is less or equals zero
+        /// - or -
+        /// the current population is less than initial population.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">Throw if the value of percents is less then 0% or more then 100%.</exception>
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
             throw new NotImplementedException("You need to implement this function.");
