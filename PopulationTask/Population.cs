282ee89b1a7c1e89c9447b5848840b7357d29cbd
Yury Hryshanau <mr.deusvult@gmail.com>
2020-11-14 22:22:54 +0300

    Up

--- PopulationTask/Population.cs
+++ PopulationTask/Population.cs
@@ -1,30 +1,48 @@
-using System;
+﻿using System;
 
 namespace PopulationTask
 {
     public static class Population
     {
-        /// <summary>
-        /// Calculates the count of years which the town need to see its population greater or equal to currentPopulation inhabitants.
-        /// </summary>
-        /// <param name="initialPopulation">The population at the beginning of a year.</param>
-        /// <param name="percent">The percentage of growth per year.</param>
-        /// <param name="visitors">The visitors (new inhabitants per year) who come to live in the town.</param>
-        /// <param name="currentPopulation">The population at present.</param>
-        /// <returns>The count of years which the town need to see its population greater or equal to currentPopulation inhabitants.</returns>
-        /// <exception cref="ArgumentException">
-        /// Thrown when initial population is less or equals 0
-        /// - or -
-        /// the count of visitors cannot be less 0
-        /// - or -
-        /// the current population is less or equals 0
-        /// - or -
-        /// the current population is less than initial population.
-        /// </exception>
-        /// <exception cref="ArgumentOutOfRangeException">Throw if the value of percents is less then 0% or more then 100%.</exception>
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (currentPopulation < 0 || currentPopulation == 0)
+            {
+                throw new ArgumentException("Parameter is less or equals zero", nameof(currentPopulation));
+            }
+
+            if (currentPopulation < initialPopulation)
+            {
+                throw new ArgumentException("Parameter is less than initialPopulation", nameof(currentPopulation));
+            }
+
+            if (initialPopulation < 0 || initialPopulation == 0)
+            {
+                throw new ArgumentException("Parameter is less or equals zero", nameof(initialPopulation));
+            }
+
+            if (percent < 0 || percent > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(percent));
+            }
+
+            if (visitors < 0)
+            {
+                throw new ArgumentException("Parameter is less zero", nameof(visitors));
+            }
+
+            var count = 1;
+            double percentPopulation = initialPopulation * percent / 100;
+            
+            double newPopulation = initialPopulation + percentPopulation + visitors;
+            while (newPopulation < currentPopulation)
+            {
+                percentPopulation = newPopulation * percent / 100;
+                newPopulation += percentPopulation + visitors;
+                count++;
+            }
+
+            return count;
         }
     }
 }
